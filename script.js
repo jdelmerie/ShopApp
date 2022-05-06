aa31e09492c9b9168c604927fddfceac2698f735
Stagiaires11 <Stagiaires11P@ad.fms-ea.com>
2022-05-06 09:40:12 +0200

    version vendredi matin

--- script.js
+++ script.js
@@ -34,17 +34,22 @@ let tapis = new Article(2, "Tapis souris", "Chaêau bleu", 5, 1);
 let articles = new Array(souris, tapis);
 
 //Variables globales
-let tbodyArticle = document.getElementById("trArticle");
+let tbodyArticle = document.getElementById("bdArticle");
 let articleBtn = document.getElementById("listeA");
 let blocArticle = document.getElementById("article");
 let tbodyCategory = document.getElementById("trCategory");
 let catBtn = document.getElementById("listeB");
 let blocCategory = document.getElementById("category");
+let articlePlace = document.getElementById("infArticle");
 
 //Affichage de la liste des articles
 articleBtn.addEventListener("click", function () {
+  tbodyArticle.replaceChildren();
   blocArticle.style.display = "block";
-  blocCategory.style.display = "none"; 
+  blocCategory.style.display = "none";
+
+  //   console.log(articles.length);
+
   articles.forEach((art) => {
     let row = document.createElement("tr");
     createTd(row, art.description);
@@ -52,6 +57,12 @@ articleBtn.addEventListener("click", function () {
     createTd(row, art.catId);
     createTd(row, art.price);
     let btnAction = document.createElement("button");
+    
+    // btnAction.innerHTML = "click me";
+    btnAction.onclick = function () {
+        addArticleToCart(art);
+      alert("hello, world");
+    };
     btnAction.innerHTML = "Ajouter";
     let action = document.createElement("td");
     action.appendChild(btnAction);
@@ -60,12 +71,12 @@ articleBtn.addEventListener("click", function () {
   });
 });
 
-
 //afichage de la liste des catégories
-catBtn.addEventListener("click", function(){
+catBtn.addEventListener("click", function () {
   blocArticle.style.display = "none";
-  blocCategory.style.display = "block"; 
-  categories.forEach(cat => {
+  blocCategory.style.display = "block";
+
+  categories.forEach((cat) => {
     let row = document.createElement("tr");
     createTd(row, cat.name);
     createTd(row, cat.description);
@@ -75,8 +86,8 @@ catBtn.addEventListener("click", function(){
     action.appendChild(btnAction);
     row.appendChild(action);
     tbodyCategory.appendChild(row);
-  })
-})
+  });
+});
 
 //create el td
 function createTd(row, text) {
@@ -84,3 +95,7 @@ function createTd(row, text) {
   td.appendChild(document.createTextNode(text));
   row.appendChild(td);
 }
+ //addArticle to cart
+ function addArticleToCart(art){
+
+ }
